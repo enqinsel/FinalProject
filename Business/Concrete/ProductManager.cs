8a0719d881086eb628978690d64cbcb24d84c06b
Engin İnsel <enqinsel@gmail.com>
2021-01-31 00:22:35 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+
+        IProductDal _prodcutDal;
+
+        public ProductManager(IProductDal prodcutDal)
+        {
+            _prodcutDal = prodcutDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            //Yetkisi var mı ?
+            return _prodcutDal.GetAll();
+
+        }
+    }
+}
